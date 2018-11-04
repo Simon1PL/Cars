2f3d1f1a5736740696b7ee918958367e3ef4a88c
Simon1PL <saicxs123@gmail.com>
2018-11-04 14:37:12 +0100

    Initial commit

--- src/test/agh/cs/lab2/RectangularMapTest.java
+++ src/test/agh/cs/lab2/RectangularMapTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 
 public class RectangularMapTest {
     @Test
-    //mlekeoeoeoeoeoeoeoeoe
     public void testCanMoveTo() {
         assertFalse(0==1);
         assertTrue(1==1);
