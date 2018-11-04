deb2d00870df4f32d471055d9001b77ed5c3becf
Simon1PL <44688394+Simon1PL@users.noreply.github.com>
2018-11-04 14:48:22 +0100

    Update UnboundedMapTest.java

--- src/test/agh/cs/lab2/UnboundedMapTest.java
+++ src/test/agh/cs/lab2/UnboundedMapTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class UnboundedMapTest {
-    //tedfasdfgasdfgsdasdgdfsdghffsfdgdfsgfghdfshfdshfds
+    //tedfasd
     @Test
     //szajs
     public void testPlace() {
